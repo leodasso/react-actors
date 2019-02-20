a11cb7f8dd082952146c3e0677ac2749da09f67a
Leo Dasso <leo.dasso@gmail.com>
2019-02-19 18:13:12 -0600

    get placeholder files in place, and theres no errors yay

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+          About
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+          About
+      </div>
+    );
+  }
+}
+
+export default Footer;
